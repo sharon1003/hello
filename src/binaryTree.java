04744e301d8b13c06095ccb1d3c589227afd11b3
sharon <SP@mail.com>
2020-03-10 13:51:38 +0800

    Initial commit

---
+++ src/binaryTree.java
@@ -0,0 +1,6 @@
+public class binaryTree {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+// Uva 127
+package com.company;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[])
+    {
+        System.out.println("hello");
+    }
+}
